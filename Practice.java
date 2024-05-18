55131a945e6aed09266c2a984a89d357496493e7
juned <79745275+junaid11P@users.noreply.github.com>
2024-05-18 20:35:08 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -2,11 +2,13 @@
 /* comment*/
 /* comment 
   comment*/
-
 public class Practice
   {
     public static void main(String[] args)
     {
-      System.out.println("Hello guys");
+      System.out.println("It will go to next line");
+      System.out.print("it won't go to next line");
+      System.out.println("first line\n second line");
+      
     }
   }
